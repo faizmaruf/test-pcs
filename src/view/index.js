883f0995cc90295bc435b1e03af7d7e1a6caa6e2
faizmaruf <faiz.maruf8888@gmail.com>
2023-02-22 15:22:29 +0700

    create pcs-test repo

---
+++ src/view/index.js
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container flex">
+        <h1 className="h1 text-red-600 text-lg m-auto">Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
